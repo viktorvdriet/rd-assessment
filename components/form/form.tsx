ae6b990898b83ef099a2d4f1b55a265d562bf494
Viktor <viktorvdriet@me.com>
2023-11-18 13:21:40 +0100

    basic form submission

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,13 +1,34 @@
 import styles from './form.module.scss'
 
-import { FC } from 'react'
+import { ChangeEvent, FC, FormEvent, useState } from 'react'
+
+type FormData = {
+  url: string;
+}
 
 const Form: FC = () => {
+  const [data, setData] = useState<FormData>({
+    url: "https://github.com/vercel/next.js"
+  });
+
+  const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  };
+
+  const handlerSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handlerSubmit}>
       <label>Please provide a URL to your public GitHub repo</label>
-      <input type="text" />
-      <button>submit</button>
+      <input type="url" name="url" onChange={inputChange} defaultValue={data.url} />
+      <button type="submit">Submit</button>
     </form>
   )
 }
